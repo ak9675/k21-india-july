b4174b73fef8b6dbcb7ed0277fd9602a7c469470
akhil <akhil@yahoo.com>
2024-07-21 21:04:25 +0530

    new commit from new folder

---
+++ main.tf
@@ -0,0 +1,22 @@
+resource "aws_eip" "name" {
+  tags = {
+    name = "${var.user-name}-eip"
+  }
+}
+
+resource "aws_s3_bucket" "myname" {
+  bucket = "${var.user-name}-buck-k21"
+}
+
+
+variable "user-name" {
+ description = "Enter your name"
+}
+
+resource "aws_instance" "myname" {
+  ami           = "ami-01b799c439fd5516a"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "${var.user-name}-instance"
+  }
+}
